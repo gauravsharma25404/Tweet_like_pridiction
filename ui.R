8767fdef2abe96fc6ebcf65ec2fd8973beb6c255
gauravsharma25404 <113029190+gauravsharma25404@users.noreply.github.com>
2023-12-09 06:03:00 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,23 +1,48 @@
-# ui.R
-
-# Load libraries
 library(shiny)
 
-# Define UI
 shinyUI(
-  fluidPage(
-    titlePanel("Political Prediction App"),
-    sidebarLayout(
-      sidebarPanel(
-        # Add any inputs or controls you need
-      ),
-      mainPanel(
-        # Output plots and other UI elements
-        plotOutput("scatterPlot"),
-        plotOutput("histogramPlot"),
-        tableOutput("modelResults")
-      )
+  navbarPage(
+    "Political Tweets",
+    tabPanel("Upload Data",
+             fluidPage(
+               titlePanel("Upload Your Dataset"),
+               sidebarLayout(
+                 sidebarPanel(
+                   fileInput("file", "Choose an Excel file", accept = c(".xls", ".xlsx"))
+                 ),
+                 mainPanel(
+                   h3("Predicted Likes Count:"),
+                   verbatimTextOutput("prediction")
+                 )
+               )
+             )
+    ),
+    tabPanel("View Dataset",
+             fluidPage(
+               titlePanel("View Dataset"),
+               fluidRow(
+                 column(12,
+                        tableOutput("fullDataset")
+                 )
+               )
+             )
+    ),
+    tabPanel("Visualization",
+             fluidPage(
+               titlePanel("Visualization Options"),
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("plotType", "Choose a plot type:",
+                               choices = c("---choose plot---", "Bar Chart", "Histogram", "Correlation Graph", "Scatter Plot", "Scatter Plot 3D", "Pie Chart")
+                   ),
+                   actionButton("plotBtn", "Plot", p = 1, width = 100),
+                   width = 8
+                 ),
+                 mainPanel(
+                   plotOutput("selectedPlot")
+                 )
+               )
+             )
     )
   )
 )
-
